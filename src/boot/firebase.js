f74d813dc0597cd230bfcef1f8ff886305019317
yagel <yagel@yagels-MacBook-Air.local>
2021-10-08 13:29:20 +0300

    first commit

---
+++ src/boot/firebase.js
@@ -0,0 +1,68 @@
+/*
+import { initializeApp } from "firebase/app";   
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAO6ZPcX-X7U1-gqmG8Bwrw3fUmraDWL30",
+  authDomain: "yagel-invest.firebaseapp.com",
+  projectId: "yagel-invest",
+  storageBucket: "yagel-invest.appspot.com",
+  messagingSenderId: "197522498925",
+  appId: "1:197522498925:web:ea864d2b9bec97b3336664",
+  measurementId: "G-X69VBQ3BRB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app); 
+
+*/
+/*
+console.log('test')
+
+
+import firebase from "firebase/app"
+import "firebase/firestore"
+console.log('test1111111')
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAO6ZPcX-X7U1-gqmG8Bwrw3fUmraDWL30",
+  authDomain: "yagel-invest.firebaseapp.com",
+  projectId: "yagel-invest",
+  storageBucket: "yagel-invest.appspot.com",
+  messagingSenderId: "197522498925",
+  appId: "1:197522498925:web:ea864d2b9bec97b3336664",
+}
+
+firebase.initializeApp(firebaseConfig)
+
+let db = firebase.firestore()
+
+export default db
+*/
+
+console.log('test2')
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app"
+import "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDNp799ecLIps1rSyuFT2tD7ZC_KGIKkvo",
+  authDomain: "qwitter-427aa.firebaseapp.com",
+  projectId: "qwitter-427aa",
+  storageBucket: "qwitter-427aa.appspot.com",
+  messagingSenderId: "143371204429",
+  appId: "1:143371204429:web:6a8fa8a4c0f2ab920ea904",
+  measurementId: "G-XKK10CW1RD"
+};
+
+
+firebase.initializeApp(firebaseConfig)
+let db = firebase.firestore()
+
+export default db
---
+++ src/pages/PageLogin.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+      {{ isGoogleLoggedin() }}
+    <button @click="login()">Login</button>
+    Is login: ? {{ isLogin }}
+    <button @click="logOut()">LogOut</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      isLogin: this.isGoogleLoggedin(),
+    };
+  },
+  methods: {
+    isGoogleLoggedin(){
+        console.log("check is logged in")
+
+        // todo: the function doesnt check the credentials to check wheter the auth is correct.
+        // In order to fix this problem: 1. use the google auth to check every time.
+        // 2. store the user id and access token in the db, and check if they the same.
+        try{
+            var gogUser = JSON.parse(this.$cookie.get('googleUser'));
+            console.log("gogUser");
+            console.log(gogUser);
+            const gogUserId = gogUser.ya;
+            const gogUserAccessToken = gogUser.$b.access_token;
+            if(gogUserId.length > 10 && gogUserAccessToken.length >10){
+                // not really checked if logged in correctly.
+                console.log("user logged in");
+                return true;
+            }
+        }catch(error){
+            return false;
+        }
+        console.log("user not logged in");
+        return false;
+    },
+
+    async logOut() {
+      const result = await this.$gAuth.signOut();
+      this.isLogin = false;
+      this.$cookie.delete('googleUser');
+      console.log(`result`, result);
+    },
+    async login() {
+      const googleUser = await this.$gAuth.signIn();
+      console.log("googleUser", googleUser);
+      this.$cookie.set('googleUser', JSON.stringify(googleUser));
+      console.log("getId", googleUser.getId());
+      console.log("getBaseProfile", googleUser.getBasicProfile());
+      console.log("getAuthResponse", googleUser.getAuthResponse());
+      console.log(
+        "getAuthResponse$G",
+        this.$gAuth.GoogleAuth.currentUser.get().getAuthResponse()
+      );
+      this.isLogin = this.$gAuth.isAuthorized;
+
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
